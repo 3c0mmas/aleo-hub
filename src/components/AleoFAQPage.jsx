69447fe9b5b5861cc146d5a0d40357f6e312fc2d
3c0mmas <143662857+3c0mmas@users.noreply.github.com>
2025-11-04 01:08:22 +0100

    Update AleoFAQPage.jsx

--- src/components/AleoFAQPage.jsx
+++ src/components/AleoFAQPage.jsx
@@ -97,7 +97,7 @@ function FloatingSpheres() {
     return () => window.removeEventListener("resize", resize);
   }, []);
 
-  return <canvas ref={canvasRef} className="absolute inset-0 w-full h-full z-0 pointer-events-none" />;
+  return <canvas ref={canvasRef} className="absolute top-0 left-0 w-full h-[800px] z-0 pointer-events-none" />;
 }
 
 // заменяем ChevronDown на встроенный SVG
